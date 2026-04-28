85fd64fcbc10e3572796bd3d226a0c4f31e28e49
Farid2302 <mm3368@srmist.edu.in>
2026-04-28 09:27:28 +0530

    Update Tictactoe.java

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,1 +1,53 @@
+import java.util.Random;
 
+public class TicTacToe {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    /**
+     * Entry point of the program. Executes the toss logic and displays
+     * the result of turn and symbol assignment.
+     */
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    /**
+     * Uses random logic to decide the first player and assigns symbols
+     * based on the toss outcome. This method initializes the game state.
+     */
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+        int toss = rand.nextInt(2); // Generates 0 or 1
+
+        // Logic: 0 = Human, 1 = Computer
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    /**
+     * Displays the toss result, indicating who plays first and which
+     * symbol is assigned to each player.
+     */
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Result: Human won the toss!");
+            System.out.println("Human will start the game with symbol: " + humanSymbol);
+        } else {
+            System.out.println("Result: Computer won the toss!");
+            System.out.println("Computer will start the game with symbol: " + computerSymbol);
+        }
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+    }
+}
